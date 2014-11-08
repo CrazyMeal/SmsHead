184dd224eae9c6f837c7c2010d87fa8564abf32c
KVN <kvndesign.mail@gmail.com>
2014-11-08 17:09:53 +0100

    Init

---
+++ src/com/crazymeal/smshead/MainActivity.java
@@ -0,0 +1,90 @@
+package com.crazymeal.smshead;
+
+import com.crazymeal.smshead.service.SmsHeadService;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Messenger;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+	private Messenger mService = null;
+	private boolean mBound;
+	
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			setmService(new Messenger(service));
+			setmBound(true);
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			setmService(null);
+			setmBound(false);
+		}
+	};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_main);
+		startService(new Intent(this.getApplicationContext(), SmsHeadService.class));
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		bindService(new Intent(this, SmsHeadService.class), mConnection, Context.BIND_AUTO_CREATE);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (mBound) {
+			unbindService(mConnection);
+			mBound = false;
+		}
+		stopService(new Intent(this.getApplicationContext(), SmsHeadService.class));
+	}
+
+	public Messenger getmService() {
+		return mService;
+	}
+
+	public void setmService(Messenger mService) {
+		this.mService = mService;
+	}
+
+	public boolean ismBound() {
+		return mBound;
+	}
+
+	public void setmBound(boolean mBound) {
+		this.mBound = mBound;
+	}
+	
+}
---
+++ trunk/src/com/crazymeal/smshead/service/SmsHeadService.java
@@ -0,0 +1,131 @@
+package com.crazymeal.smshead.service;
+
+import com.crazymeal.smshead.R;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class SmsHeadService extends Service{
+	
+	private WindowManager windowManager;
+	private ImageView smsHead, deleteZone;
+	private LayoutParams params, deleteParams;
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		Toast.makeText(getApplicationContext(), "Service launched", Toast.LENGTH_SHORT).show();
+		return null;
+	}
+
+	@Override public void onCreate() {
+	    super.onCreate();
+
+	    this.windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+	    this.smsHead = new ImageView(this);
+	    this.smsHead.setImageResource(R.drawable.ic_message);
+
+	    this.deleteZone = new ImageView(this);
+	    this.deleteZone.setImageResource(R.drawable.ic_delete);
+	    this.deleteZone.setVisibility(View.INVISIBLE);
+	    
+	    this.initParameters();
+	    
+	    this.smsHead.setOnTouchListener(new View.OnTouchListener() {
+	    	  private int initialX;
+	    	  private int initialY;
+	    	  private float initialTouchX;
+	    	  private float initialTouchY;
+
+			@Override public boolean onTouch(View v, MotionEvent event) {
+	    		  deleteZone.setVisibility(View.VISIBLE);
+	    		  switch (event.getAction()) {
+		    	      case MotionEvent.ACTION_DOWN:
+		    	        initialX = params.x;
+		    	        initialY = params.y;
+		    	        initialTouchX = event.getRawX();
+		    	        initialTouchY = event.getRawY();
+		    	        return true;
+		    	      case MotionEvent.ACTION_UP:
+		    	    	  if(checkIfHovered()){
+		    	    		  smsHead.setVisibility(View.INVISIBLE);
+		    	    		  windowManager.removeView(smsHead);
+		    	    	  }
+		    	    	  deleteZone.setVisibility(View.INVISIBLE);
+		    	        return true;
+		    	      case MotionEvent.ACTION_MOVE:
+		    	    	checkIfHovered();
+		    	        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+		    	        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+		    	        windowManager.updateViewLayout(smsHead, params);
+		    	        return true;
+	    	    }
+	    	    return false;
+	    	  }
+	    	});
+	    this.windowManager.addView(this.deleteZone, this.deleteParams);
+	    this.windowManager.addView(this.smsHead, this.params);
+	  }
+	
+	@SuppressLint("NewApi")
+	protected boolean checkIfHovered() {
+		boolean hovered = false;
+		if((this.params.x > (this.deleteParams.x - this.smsHead.getWidth()/2)) && (this.params.x < (this.deleteParams.x + this.deleteZone.getWidth() - this.smsHead.getWidth()/2))){
+			if((this.params.y > (this.deleteParams.y - this.smsHead.getHeight()/2)) && (this.params.y < (this.deleteParams.y + this.deleteZone.getHeight() - this.smsHead.getHeight()/2)))
+				hovered = true;;
+		}
+		return hovered;
+	}
+
+	@Override public void onDestroy(){
+		super.onDestroy();
+		if(smsHead != null) this.windowManager.removeView(this.smsHead);
+		if(deleteZone != null) this.windowManager.removeView(this.deleteZone);
+	}
+	
+	@SuppressLint("NewApi")
+	public void initParameters(){
+		this.params = new WindowManager.LayoutParams(
+		        WindowManager.LayoutParams.WRAP_CONTENT,
+		        WindowManager.LayoutParams.WRAP_CONTENT,
+		        WindowManager.LayoutParams.TYPE_PHONE,
+		        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+		        PixelFormat.TRANSLUCENT);
+		    this.deleteParams = new WindowManager.LayoutParams(
+			        WindowManager.LayoutParams.WRAP_CONTENT,
+			        WindowManager.LayoutParams.WRAP_CONTENT,
+			        WindowManager.LayoutParams.TYPE_PHONE,
+			        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+			        PixelFormat.TRANSLUCENT);
+		    
+		    params.gravity = Gravity.TOP | Gravity.LEFT;
+		    params.x = 0;
+		    params.y = 100;
+		    
+		    
+		    Display display = this.windowManager.getDefaultDisplay();
+		    final Point size = new Point();
+		    display.getSize(size);
+		    float centerY = size.y/2;//i expect this is the y coordinate of center
+		    float centerX = size.x/2;
+		    
+		    deleteParams.gravity = Gravity.TOP | Gravity.LEFT;
+		    deleteParams.x = (int) centerX - 64;
+		    deleteParams.y = (int) centerY - 64;
+	}
+}
