c267446d7338a83a788c5141b87ec855da7cae55
KVN <kvndesign.mail@gmail.com>
2014-11-10 19:07:33 +0100

    Add new View on sms reveived

--- src/com/crazymeal/smshead/service/SmsHeadService.java
+++ src/com/crazymeal/smshead/service/SmsHeadService.java
@@ -5,10 +5,15 @@ import java.util.Map.Entry;
 
 import android.annotation.SuppressLint;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.telephony.SmsMessage;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -39,6 +44,7 @@ public class SmsHeadService extends Service{
 	@Override public void onCreate() {
 	    super.onCreate();
 	    this.viewList = new HashMap<View, WindowManager.LayoutParams>();
+	    
 	    
 	    this.smsHead = LayoutInflater.from(this).inflate(R.layout.sms_head_layout, null);
 	    TextView messageView = (TextView) this.smsHead.findViewById(R.id.textView_message);
@@ -94,14 +100,30 @@ public class SmsHeadService extends Service{
 	    //this.windowManager.addView(this.smsHead, this.params);
 	  }
 	
+	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		for(Entry<View, LayoutParams> v : this.viewList.entrySet()){
 			this.windowManager.addView(v.getKey(), v.getValue());
 		}
+		
+		//SmsReceiver r = new SmsReceiver();
+		
+		IntentFilter filter = new IntentFilter();
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        filter.addCategory("com.crazymeal.smshead");
+        this.registerReceiver(new SmsReceiver(), filter);
+        
 		return super.onStartCommand(intent, flags, startId);
 	}
 
+	@Override public void onDestroy(){
+		super.onDestroy();
+		if(smsHead != null) this.windowManager.removeView(this.smsHead);
+		if(deleteZone != null) this.windowManager.removeView(this.deleteZone);
+	}
+	
 	@SuppressLint("NewApi")
 	protected boolean checkIfHovered() {
 		boolean hovered = false;
@@ -110,12 +132,6 @@ public class SmsHeadService extends Service{
 				hovered = true;;
 		}
 		return hovered;
-	}
-
-	@Override public void onDestroy(){
-		super.onDestroy();
-		if(smsHead != null) this.windowManager.removeView(this.smsHead);
-		if(deleteZone != null) this.windowManager.removeView(this.deleteZone);
 	}
 	
 	@SuppressLint("NewApi")
@@ -148,8 +164,69 @@ public class SmsHeadService extends Service{
 		    deleteParams.x = (int) centerX - 64;
 		    deleteParams.y = (int) centerY - 64;
 	}
+	public void addView(String sender, String message){
+		LayoutParams tmpParams = new WindowManager.LayoutParams(
+		        WindowManager.LayoutParams.WRAP_CONTENT,
+		        WindowManager.LayoutParams.WRAP_CONTENT,
+		        WindowManager.LayoutParams.TYPE_PHONE,
+		        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+		        PixelFormat.TRANSLUCENT);
+		tmpParams.gravity = Gravity.TOP | Gravity.LEFT;
+		tmpParams.x = 0;
+		tmpParams.y = 300;
+	    
+		View tmpView = LayoutInflater.from(this).inflate(R.layout.sms_head_layout, null);
+	    TextView messageView = (TextView) tmpView.findViewById(R.id.textView_message);
+	    TextView senderView = (TextView) tmpView.findViewById(R.id.textView_sender);
+	    senderView.setText(sender);
+	    messageView.setText(message);
+		
+		this.viewList.put(tmpView, tmpParams);
+		this.windowManager.addView(tmpView, tmpParams);
+	}
 	private void removeView(View view){
 		this.windowManager.removeView(view);
 		this.viewList.remove(view);
+	}
+	
+	class SmsReceiver extends BroadcastReceiver{
+		private String sender, message;
+		
+		@SuppressLint("NewApi")
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			// ---get the SMS message passed in---
+	        Bundle bundle = intent.getExtras();
+	        SmsMessage[] msgs = null;
+	        String str = "";
+	        if (bundle != null) {
+	            // ---retrieve the SMS message received---
+	        	String sender = "", message = "";
+	            Object[] pdus = (Object[]) bundle.get("pdus");
+	            msgs = new SmsMessage[pdus.length];
+	            for (int i = 0; i < msgs.length; i++) {
+	                msgs[i] = SmsMessage.createFromPdu((byte[])    pdus[i]);
+	                str += "SMS from " + msgs[i].getOriginatingAddress();
+	                str += " :";
+	                str += msgs[i].getMessageBody().toString();
+	                str += "\n";
+	                
+	                sender += msgs[i].getOriginatingAddress();
+	                message += msgs[i].getMessageBody().toString();
+	            }
+	            Toast.makeText(context, "sender: " + sender + "mes: " + message, Toast.LENGTH_SHORT).show();
+	            addView(sender, message);
+	            // ---display the new SMS message---
+	        }
+		}
+		
+		public String getSender() {
+			return sender;
+		}
+
+		public String getMessage() {
+			return message;
+		}
 	}
 }
